85cbcb6fed6799232fcd75fb0af1360af06b3d5e
Qamrul-Hassan <sajal700@gmail.com>
2025-04-17 02:45:45 +0600

    Update README with project details

--- components/animations/SunnyAnimation.js
+++ components/animations/SunnyAnimation.js
@@ -1,30 +1,14 @@
-"use client"; // Ensure this is a Client Component
-import { useEffect, useRef } from "react";
-
+"use client";
 const SunnyAnimation = () => {
-  const sunRef = useRef(null);
-
-  useEffect(() => {
-    const animateSun = () => {
-      // Apply continuous rotation
-      if (sunRef.current) {
-        sunRef.current.style.animation = "spin 20s linear infinite";
-      }
-    };
-    animateSun();
-  }, []);
-
   return (
     <div className="relative w-full h-48 flex items-center justify-center">
-      {/* Sun  color */}
       <div
-        ref={sunRef}
-        className="w-24 h-24 bg-yellow-200 rounded-full shadow-3xl"
+        className="w-24 h-24 bg-yellow-200 rounded-full shadow-3xl animate-spin"
         style={{
+          animationDuration: "20s",
           boxShadow: "0 0 50px 20px rgba(255, 223, 0, 0.5)",
         }}
       >
-        {/* Sun Rays with 3D Effect and Gradient */}
         {[...Array(8)].map((_, i) => (
           <div
             key={i}
@@ -36,7 +20,7 @@ const SunnyAnimation = () => {
               background: `linear-gradient(45deg, rgba(255, 223, 0, 0.9), rgba(255, 165, 0, 0.9), rgba(255, 69, 0, 0.9))`,
               boxShadow: `0 0 40px rgba(255, 165, 0, 0.8), 0 0 50px rgba(255, 69, 0, 0.5)`,
             }}
-          ></div>
+          />
         ))}
       </div>
     </div>
