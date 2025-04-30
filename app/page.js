a6203800443240102ef7f20f73e1f6f4eeed0480
Qamrul-Hassan <sajal700@gmail.com>
2025-05-01 01:18:38 +0600

    Added Title and Icon

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Weather from "../components/Weather";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-200 px-4 sm:px-6 md:px-8 py-2">
-      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-500 to-slate-600 mb-8 text-center animate__animated animate__fadeIn">
+      <h1 className="flex items-center text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-500 to-slate-600 mb-8 text-center animate__animated animate__fadeIn">
+        <span className="text-yellow-400 text-5xl mr-4">☀️</span>
         SkyCast Weather
       </h1>
       <div className="w-full max-w-4xl">
