5635e5d48b828768ad0a70aae51806b333309e1c
Qamrul-Hassan <sajal700@gmail.com>
2025-05-01 01:04:26 +0600

    Added Title

--- app/page.js
+++ app/page.js
@@ -2,7 +2,10 @@ import Weather from "../components/Weather";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-200 px-4 sm:px-6 md:px-8">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-200 px-4 sm:px-6 md:px-8 py-2">
+      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-slate-500 to-slate-600 mb-8 text-center animate__animated animate__fadeIn">
+        SkyCast Weather
+      </h1>
       <div className="w-full max-w-4xl">
         <Weather />
       </div>
